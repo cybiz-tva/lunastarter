5dc107c7263aec51b3807559a3c1b1f5e5150497
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2022-08-12 10:18:32 +0530

    Update AboutSection.js

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -10,13 +10,13 @@ export default function AboutSection({ logoBig }) {
       </div>
       <div className="about__section__right">
         <div className="about__section__right__heading">
-          What is Lunastarter?
+          What is Stoverse?
         </div>
         <div className="about__section__right__info">
-          Lunastarter is a community-driven, tier-less launchpad and incubator
+          Stoverse is a community-driven, tier-less launchpad and incubator
           built on Terra blockchain. It has a very unique community-driven
           project selection mechanism with a fair and guaranteed allocation for
-          all of the participants. Lunastarter guides entrepreneurs to find
+          all of the participants. Stoverse guides entrepreneurs to find
           innovative business models to maximize their impact and develop their
           projects as financially viable investments.
         </div>
