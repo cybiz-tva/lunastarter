16407378a41db6a8ed2d556a32d210b08ec60bb3
cybiz <49467492+cybiz-tva@users.noreply.github.com>
2022-08-25 00:39:54 +0530

    Update idonew.js

--- src/screens/idonew.js
+++ src/screens/idonew.js
@@ -1,1 +1,9 @@
-  fetch('https://atoll-ido-five.vercel.app/')
+import { Route, Redirect } from 'react-router'
+
+<Route exact path="/" render={() => (
+  loggedIn ? (
+    <Redirect to="https://atoll-ido-five.vercel.app/"/>
+  ) : (
+    <PublicHomePage/>
+  )
+)}/>
